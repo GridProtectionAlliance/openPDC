52b0b8ddae3144ce4b89a791be48b4ed470f5c0f
pinalcpatel <pcpatel@tva.gov>
2009-10-07 19:59:13 +0000

    Synchrophasor: Version change for build Synchrophasor_v1.0.5.31272.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C31281

--- Source/Tools/PMUConnectionTester/PMUConnectionTester/My Project/AssemblyInfo.vb
+++ Source/Tools/PMUConnectionTester/PMUConnectionTester/My Project/AssemblyInfo.vb
@@ -2,7 +2,7 @@
 Imports System.Runtime.InteropServices
 
 ' Assembly identity attributes.
-<Assembly: AssemblyVersion("1.0.4.31271")> 
+<Assembly: AssemblyVersion("1.0.5.31272")> 
 
 ' Informational attributes.
 <Assembly: AssemblyCompany("TVA")> 
