f89f6425f017a9906a182f66f749ff1a7ca8a93b
pinalcpatel <pcpatel@tva.gov>
2009-10-09 14:19:55 +0000

    Synchrophasor: Version change for build Synchrophasor_v1.0.6.31281.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C31486

--- Source/Libraries/TVA.PhasorProtocols/Properties/AssemblyInfo.cs
+++ Source/Libraries/TVA.PhasorProtocols/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using System.Security.Permissions;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.0.5.31272")]
+[assembly: AssemblyVersion("1.0.6.31281")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("TVA")]
--- Source/Tools/PMUConnectionTester/PMUConnectionTester/My Project/AssemblyInfo.vb
+++ Source/Tools/PMUConnectionTester/PMUConnectionTester/My Project/AssemblyInfo.vb
@@ -2,7 +2,7 @@
 Imports System.Runtime.InteropServices
 
 ' Assembly identity attributes.
-<Assembly: AssemblyVersion("1.0.5.31272")> 
+<Assembly: AssemblyVersion("1.0.6.31281")> 
 
 ' Informational attributes.
 <Assembly: AssemblyCompany("TVA")> 
