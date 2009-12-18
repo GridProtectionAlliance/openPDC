53a03917ba8546fda2760484cfb0ba7117bbf335
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2009-12-18 18:55:48 +0000

    Synchrophasor: Updated loss of field measurement I/O definitions.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C39375

--- Source/Libraries/Adapters/LOFTrigger/LOFTrigger.cs
+++ Source/Libraries/Adapters/LOFTrigger/LOFTrigger.cs
@@ -251,7 +251,6 @@ namespace LOFTrigger
         #region [ Members ]
 
         // Fields
-        private ProcessQueue<IFrame> m_processQueue;
         private double m_PSet;                                     //Threshold of Pset MW: default value -600 mW      
         private double m_QSet;                                     //Threshold of Qset MVar: default value 200 mVar
         private double m_QAreaSet;                                 //Threshold of Qarea MVar-sec: default value 500 mVar-sec
@@ -266,11 +265,7 @@ namespace LOFTrigger
         MeasurementKey m_MontVoltageAngle;
         MeasurementKey m_MontCurrentMagnitude;
         MeasurementKey m_MontCurrentAngle;
-
-
-        #endregion
-
-        #region [ Constructors ]
+        IMeasurement m_warningSignal;
 
         #endregion
 
@@ -297,35 +292,8 @@ namespace LOFTrigger
         #region [ Methods ]
 
         /// <summary>
-        /// Releases the unmanaged resources used by the <see cref="FrequencyMonitor"/> object and optionally releases the managed resources.
-        /// </summary>
-        /// <param name="disposing">true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (!m_disposed)
-            {
-                try
-                {
-                    if (disposing)
-                    {
-                        if (m_processQueue != null)
-                            m_processQueue.Dispose();
-
-                        m_processQueue = null;
-                    }
-                }
-                finally
-                {
-                    base.Dispose(disposing);    // Call base class Dispose().
-                    m_disposed = true;          // Prevent duplicate dispose.
-                }
-            }
-        }
-
-        /// <summary>
         /// Initializes <see cref="FrequencyMonitor"/>.
         /// </summary>
-
         public override void Initialize()
         {
             base.Initialize();
@@ -345,36 +313,21 @@ namespace LOFTrigger
             m_count1 = 0;
             m_count2 = 0;
 
-
             //m_PSet = -600;
             //m_QSet = 200;
             //m_QAreaSet = 500;
             //m_VThreshold = 475000;
             //m_interval = 30;
 
-            m_MontVoltageMagnitude = new MeasurementKey(3981, "P2");
-            m_MontVoltageAngle = new MeasurementKey(3980, "P2");
-            m_MontCurrentMagnitude = new MeasurementKey(3991, "P2");
-            m_MontCurrentAngle = new MeasurementKey(3990, "P2");
-            m_processQueue = ProcessQueue<IFrame>.CreateRealTimeQueue(ProcessFrames);
-        }
+            // Load needed measurement keys from defined InputMeasurementKeys
+            // inputMeasurementKeys={P2:3981;P2:3980;P2:3991;P2:3990}; outputMeasurements={P2:9999}
+            m_MontVoltageMagnitude = InputMeasurementKeys[InputMeasurementKeyTypes.IndexOf(signalType => signalType == SignalType.VPHM)];
+            m_MontVoltageAngle = InputMeasurementKeys[InputMeasurementKeyTypes.IndexOf(signalType => signalType == SignalType.VPHA)];
+            m_MontCurrentMagnitude = InputMeasurementKeys[InputMeasurementKeyTypes.IndexOf(signalType => signalType == SignalType.IPHM)];
+            m_MontCurrentAngle = InputMeasurementKeys[InputMeasurementKeyTypes.IndexOf(signalType => signalType == SignalType.IPHA)];
 
-        /// <summary>
-        /// Starts the <see cref="FrequencyMonitor"/>, if it is not already running.
-        /// </summary>
-        public override void Start()
-        {
-            base.Start();
-            m_processQueue.Start();
-        }
-
-        /// <summary>
-        /// Stops the <see cref="FrequencyMonitor"/>.
-        /// </summary>
-        public override void Stop()
-        {
-            m_processQueue.Stop();
-            base.Stop();
+            // Load warning signal output measurement
+            m_warningSignal = OutputMeasurements[0];
         }
 
         /// <summary>
@@ -390,83 +343,72 @@ namespace LOFTrigger
         /// </remarks>
         protected override void PublishFrame(IFrame frame, int index)
         {
-            m_processQueue.Add(frame);
-        }
-
-
-        /// <summary>
-        /// Process queued frames
-        /// </summary>
-        /// <param name="frames"></param>
-        protected void ProcessFrames(IFrame[] frames)
-        {
-            double voltageMagnitude;
-            double voltageAngle;
-            double currentMagnitude;
-            double currentAngle;
+            double voltageMagnitude = 0.0D;
+            double voltageAngle = 0.0D;
+            double currentMagnitude = 0.0D;
+            double currentAngle = 0.0D;
             double realPower;
             double reactivePower;
             double deltaT;
             IMeasurement measurement;
-            foreach (IFrame frame in frames)
+            bool warningSignaled = false;
+
+            m_count++;
+#if DEBUG
+            if (m_count % 60 == 0)
+                OnStatusMessage("{0} events handled...", m_count);
+#endif
+            if ((m_count % m_interval) == 0)
             {
-                m_count++;
-#if DEBUG
-                if (m_count % 60 == 0)
-                    OnStatusMessage("{0} events handled...", m_count);
-#endif
-                if ((m_count % m_interval) == 0)
+                m_count1 = m_count2;
+                m_count2 = m_count;
+
+                if (frame.Measurements.TryGetValue(m_MontVoltageMagnitude, out measurement))
+                    voltageMagnitude = measurement.AdjustedValue;                           
+                else
+                    OnProcessException(new InvalidOperationException("Failed to receive voltage magnitude - could not calculate loss of field."));
+
+                if (frame.Measurements.TryGetValue(m_MontVoltageAngle, out measurement))
+                    voltageAngle = measurement.AdjustedValue / 180.00 * Math.PI;
+                else
+                    OnProcessException(new InvalidOperationException("Failed to receive voltage angle - could not calculate loss of field.."));
+
+                if (frame.Measurements.TryGetValue(m_MontCurrentMagnitude, out measurement))
+                    currentMagnitude = measurement.AdjustedValue;
+                else
+                    OnProcessException(new InvalidOperationException("Failed to receive current magnitude - could not calculate loss of field.."));
+
+                if (frame.Measurements.TryGetValue(m_MontCurrentAngle, out measurement))
+                    currentAngle = measurement.AdjustedValue / 180.00 * Math.PI;                       
+                else
+                    OnProcessException(new InvalidOperationException("Failed to receive current angle - could not calculate loss of field.."));
+
+                realPower = 3 * voltageMagnitude * currentMagnitude * Math.Cos(voltageAngle - currentAngle) / 1000000.00;
+                reactivePower = 3 * voltageMagnitude * currentMagnitude * Math.Sin(voltageAngle - currentAngle) / 1000000.00;
+                deltaT = (m_count2 - m_count1) / FramesPerSecond;
+
+                if ((realPower < m_PSet) && (reactivePower > m_QSet))
                 {
-                    m_count1 = m_count2;
-                    m_count2 = m_count;
-                    try
+                    m_QAreamVar = m_QAreamVar + deltaT * (reactivePower - m_QSet);
+                    if ((m_QAreamVar > m_QAreaSet) && (voltageMagnitude < (m_VThreshold / Math.Sqrt(3))))
                     {
-                        if (frame.Measurements.TryGetValue(m_MontVoltageMagnitude, out measurement))
-                            voltageMagnitude = measurement.AdjustedValue;                           
-                        else
-                            continue;
-                        if (frame.Measurements.TryGetValue(m_MontVoltageAngle, out measurement))
-                            voltageAngle = measurement.AdjustedValue / 180.00 * Math.PI;
-                        else
-                            continue;
-                        if (frame.Measurements.TryGetValue(m_MontCurrentMagnitude, out measurement))
-                            currentMagnitude = measurement.AdjustedValue;
-                        else
-                            continue;
-                        if (frame.Measurements.TryGetValue(m_MontCurrentAngle, out measurement))
-                            currentAngle = measurement.AdjustedValue / 180.00 * Math.PI;                       
-                        else
-                            continue;
-
-                        realPower = 3 * voltageMagnitude * currentMagnitude * Math.Cos(voltageAngle - currentAngle) / 1000000.00;
-                        reactivePower = 3 * voltageMagnitude * currentMagnitude * Math.Sin(voltageAngle - currentAngle) / 1000000.00;
-                        deltaT = (m_count2 - m_count1) / FramesPerSecond;
-
-                        if ((realPower < m_PSet) && (reactivePower > m_QSet))
-                        {
-                            m_QAreamVar = m_QAreamVar + deltaT * (reactivePower - m_QSet);
-                            if ((m_QAreamVar > m_QAreaSet) && (voltageMagnitude < (m_VThreshold / Math.Sqrt(3))))
-                            {
-                                OutputLOFWarning(realPower, reactivePower, m_QAreamVar);
-                            }
-                        }
-                        else
-                            m_QAreamVar = 0;
-#if DEBUG
-                        OnStatusMessage("In ProcessFrames: MontVoltageMagnitude is {0}", voltageMagnitude);
-                        OnStatusMessage("In ProcessFrames: MontVoltageAngle is {0}", voltageAngle);
-                        OnStatusMessage("In ProcessFrames: MontCurrentMagnitude is {0}", currentMagnitude);
-                        OnStatusMessage("In ProcessFrames: MontCurrentAngle is {0}", currentAngle);
-                        OnStatusMessage("In ProcessFrames: RealPower is {0};ReactivePower is {1}; DeltaT is {2}", realPower, reactivePower, deltaT);
-                        OnStatusMessage("In ProcessFrames: m_QAreamVar is {0}", m_QAreamVar);
-#endif
-                    }
-                    catch (Exception ex)
-                    {
-                        OnStatusMessage(string.Format("***LOFTrigger:{0} ***", ex.ToString()));
+                        warningSignaled = true;
+                        OutputLOFWarning(realPower, reactivePower, m_QAreamVar);
                     }
                 }
+                else
+                    m_QAreamVar = 0;
+#if DEBUG
+                OnStatusMessage("In ProcessFrames: MontVoltageMagnitude is {0}", voltageMagnitude);
+                OnStatusMessage("In ProcessFrames: MontVoltageAngle is {0}", voltageAngle);
+                OnStatusMessage("In ProcessFrames: MontCurrentMagnitude is {0}", currentMagnitude);
+                OnStatusMessage("In ProcessFrames: MontCurrentAngle is {0}", currentAngle);
+                OnStatusMessage("In ProcessFrames: RealPower is {0};ReactivePower is {1}; DeltaT is {2}", realPower, reactivePower, deltaT);
+                OnStatusMessage("In ProcessFrames: m_QAreamVar is {0}", m_QAreamVar);
+#endif
 
+                // Expose warning signal value
+                OnNewMeasurements(new IMeasurement[] {Measurement.Clone(m_warningSignal, warningSignaled ? 1.0D : 0.0D, frame.Timestamp)});
             }
         }
 
