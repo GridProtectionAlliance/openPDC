a475a374231c667b00343b4b27f417a10dce64df
pinalcpatel <pcpatel@tva.gov>
2009-12-23 05:01:48 +0000

    Synchrophasor: Version change for build Synchrophasor_v1.0.63.39524.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C39530

--- Source/Applications/openPDCManager/Silverlight/Properties/AssemblyInfo.cs
+++ Source/Applications/openPDCManager/Silverlight/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.62.39493")]
+[assembly: AssemblyVersion("1.0.63.39524")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- Source/Tools/PMUConnectionTester/PMUConnectionTester/My Project/AssemblyInfo.vb
+++ Source/Tools/PMUConnectionTester/PMUConnectionTester/My Project/AssemblyInfo.vb
@@ -2,7 +2,7 @@
 Imports System.Runtime.InteropServices
 
 ' Assembly identity attributes.
-<Assembly: AssemblyVersion("1.0.62.39493")> 
+<Assembly: AssemblyVersion("1.0.63.39524")> 
 
 ' Informational attributes.
 <Assembly: AssemblyCompany("TVA")> 
