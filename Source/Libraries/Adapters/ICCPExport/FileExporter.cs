ed86270dbbd8b3e1465a2b7a937961b6fc84f3be
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2010-01-27 20:19:23 +0000

    Synchrophasor: Checking for null input measurement keys in ICCP exporter.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C40824

--- Source/Libraries/Adapters/ICCPExport/FileExporter.cs
+++ Source/Libraries/Adapters/ICCPExport/FileExporter.cs
@@ -321,7 +321,7 @@ namespace ICCPExport
             string errorMessage = "{0} is missing from Settings - Example: exportInterval=5; useReferenceAngle=True; referenceAngleMeasurement=ArchiveA:121; companyTagPrefix=TVA; useNumericQuality=True; inputMeasurementKeys={FILTER ActiveMeasurements WHERE Device='SHELBY' AND SignalType='FREQ'}";
             string setting;
 
-            if (InputMeasurementKeys.Length == 0)
+            if (InputMeasurementKeys == null || InputMeasurementKeys.Length == 0)
                 throw new InvalidOperationException("There are no input measurements defined. You must define \"inputMeasurementKeys\" to define which measurements to export");
 
             // Load required parameters
