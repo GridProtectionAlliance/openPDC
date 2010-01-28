6bb3c78402aa7563da429818e9f791fb6d674c8c
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2010-01-28 13:57:07 +0000

    Synchrophasor: Version change for build Synchrophasor_v1.0.75.40855.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C40857

--- Source/Applications/openPDC/openPDCConsole/Properties/AssemblyInfo.cs
+++ Source/Applications/openPDC/openPDCConsole/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.0.74.40824")]
+[assembly: AssemblyVersion("1.0.75.40855")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("TVA")]
--- Source/Libraries/Adapters/TestingAdapters/Properties/AssemblyInfo.cs
+++ Source/Libraries/Adapters/TestingAdapters/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using System.Security.Permissions;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.0.74.40824")]
+[assembly: AssemblyVersion("1.0.75.40855")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("TVA")]
