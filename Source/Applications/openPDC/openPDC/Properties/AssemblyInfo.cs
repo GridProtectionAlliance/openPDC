61ae36fb5748157a2e1811436d40f15e41a61f44
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2010-03-09 05:01:28 +0000

    Synchrophasor: Version change for build Synchrophasor_v1.1.14.42233.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C42243

--- Source/Applications/openPDC/openPDC/Properties/AssemblyInfo.cs
+++ Source/Applications/openPDC/openPDC/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.1.13.42159")]
+[assembly: AssemblyVersion("1.1.14.42233")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("TVA")]
--- Source/Tools/PMUConnectionTester/PMUConnectionTester/My Project/AssemblyInfo.vb
+++ Source/Tools/PMUConnectionTester/PMUConnectionTester/My Project/AssemblyInfo.vb
@@ -2,7 +2,7 @@
 Imports System.Runtime.InteropServices
 
 ' Assembly identity attributes.
-<Assembly: AssemblyVersion("1.1.13.42159")> 
+<Assembly: AssemblyVersion("1.1.14.42233")> 
 
 ' Informational attributes.
 <Assembly: AssemblyCompany("TVA")> 
