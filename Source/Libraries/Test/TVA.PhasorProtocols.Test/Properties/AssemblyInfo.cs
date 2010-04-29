5f3899e366542e9f0c294ea74f278e1585fcda4a
pinalcpatel <pcpatel@tva.gov>
2010-04-29 15:19:36 +0000

    Synchrophasor: Created initial structure for unit testing.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C46887

---
+++ Source/Libraries/Test/TVA.PhasorProtocols.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Assembly identity attributes.
+[assembly: AssemblyVersion("1.0.0.0")]
+
+// Informational attributes.
+[assembly: AssemblyCompany("TVA")]
+[assembly: AssemblyCopyright("No copyright is claimed pursuant to 17 USC § 105.  All Other Rights Reserved.")]
+[assembly: AssemblyProduct("openPDC")]
+
+// Assembly manifest attributes.
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug Build")]
+#else
+[assembly: AssemblyConfiguration("Release Build")]
+#endif
+[assembly: AssemblyDefaultAlias("TVA.PhasorProtocols.Test")]
+[assembly: AssemblyDescription("Unit tests for phasor protocol implementations.")]
+[assembly: AssemblyTitle("TVA.PhasorProtocols.Test")]
+
+// Other configuration attributes.
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("b9b81fad-e00e-4cb0-a5cf-133007d31cb5")]
