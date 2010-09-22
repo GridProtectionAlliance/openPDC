dc1b9c0b7d25f70c7fde2a7deeafbe7d16bee57e
mbrahmbhatt <mihir9274@yahoo.com>
2010-09-22 17:03:30 +0000

    Synchrophasor: Version change for build Synchrophasor_v1.2.46.57530.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C57531

--- Source/Applications/openPDC/openPDCServices/Properties/AssemblyInfo.cs
+++ Source/Applications/openPDC/openPDCServices/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.2.45.57525")]
+[assembly: AssemblyVersion("1.2.46.57530")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
--- Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.2.45.57525")]
-[assembly: AssemblyVersion("1.2.45.57525")]
+// [assembly: AssemblyVersion("1.2.46.57530")]
+[assembly: AssemblyVersion("1.2.46.57530")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
