f12fc5c1d22807b0ee3896753c1aaf78af04b389
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2010-09-22 11:50:16 +0000

    Synchrophasor: Updated baser setup utility version number.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C57514

--- Source/Tools/Setup/Properties/AssemblyInfo.cs
+++ Source/Tools/Setup/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.2.40.57491")]
 [assembly: AssemblyVersion("1.2.40.57491")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
