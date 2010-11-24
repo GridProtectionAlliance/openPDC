35bfa1296b7d4d1dc1784cd1a6910e8808746324
pinalcpatel <pcpatel@tva.gov>
2010-11-24 20:51:30 +0000

    Synchrophasor: Version change for build Synchrophasor_v1.4.26.60102.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C60103

--- Source/Applications/openPDCManager/UserControls/Properties/AssemblyInfo.cs
+++ Source/Applications/openPDCManager/UserControls/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.4.25.59661")]
-[assembly: AssemblyVersion("1.4.25.59661")]
+// [assembly: AssemblyVersion("1.4.26.60102")]
+[assembly: AssemblyVersion("1.4.26.60102")]
 [assembly: AssemblyFileVersion("1.3.11.58011${AfterVersion}]
--- Source/Libraries/Adapters/NASPInet/Properties/AssemblyInfo.cs
+++ Source/Libraries/Adapters/NASPInet/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using System.Security.Permissions;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.4.25.59661")]
+[assembly: AssemblyVersion("1.4.26.60102")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
