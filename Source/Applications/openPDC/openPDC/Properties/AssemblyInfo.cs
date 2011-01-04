1f31c875d7a7afbe8a2cd31478187eb4d503490e
pinalcpatel <pcpatel@tva.gov>
2011-01-04 05:08:06 +0000

    Synchrophasor: Version change for build Synchrophasor_v1.4.54.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C61736

--- Source/Applications/openPDC/openPDC/Properties/AssemblyInfo.cs
+++ Source/Applications/openPDC/openPDC/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.4.53.0")]
+[assembly: AssemblyVersion("1.4.54.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
