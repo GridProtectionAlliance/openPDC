ce534984cb1d56c2574be793a36e2f0366db84d9
mthakkar <hello_mehul@yahoo.com>
2011-07-20 19:06:13 +0000

    Synchrophasor: 1) Fixed xaml error on Input Wizard user control. 2) Updated TimeSeriesFramework dependencies.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C69015

---
+++ Source/Libraries/openPDCManager.UI/DataModels/Statistic.cs
@@ -0,0 +1,88 @@
+﻿//******************************************************************************************************
+//  Statistic.cs - Gbtc
+//
+//  Copyright © 2010, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the Eclipse Public License -v 1.0 (the "License"); you may
+//  not use this file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://www.opensource.org/licenses/eclipse-1.0.php
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  07/20/2011 - Mehulbhai P Thakkar
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using TimeSeriesFramework.UI;
+
+namespace openPDCManager.UI.DataModels
+{
+    /// <summary>
+    /// Represents a record of <see cref="Statistic"/> information as defined in the database.
+    /// </summary>
+    public class Statistic : DataModelBase
+    {
+
+        #region [ Members ]
+
+        // Fields
+        private int m_id;
+        private string m_source;
+        private int m_signalIndex;
+        private string m_name;
+        private string m_description;
+        private string m_assemblyName;
+        private string m_typeName;
+        private string m_methodName;
+        private string m_arguments;
+        private bool m_enabled;
+        private string m_dataType;
+        private string m_displayFormat;
+        private bool m_isConnectedState;
+        private int m_loadOrder;
+        //private string m_value;
+        //private string m_timeTag;
+        //private string m_quality;
+
+        #endregion
+
+        #region [ Properties ]
+
+
+
+        #endregion
+
+        #region [ Constructors ]
+
+        #endregion
+
+        #region [ Methods ]
+
+        #endregion
+
+        #region [ Operators ]
+
+        #endregion
+
+        #region [ Static ]
+
+        // Static Fields
+
+        // Static Constructor
+
+        // Static Properties
+
+        // Static Methods
+
+        #endregion
+
+    }
+}
