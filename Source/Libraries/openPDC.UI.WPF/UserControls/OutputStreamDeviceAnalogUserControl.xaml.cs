1a97d9a53ba8769098e7a102b512744b10aaf7b1
salveraniket <salver.aniket@gmail.com>
2011-09-12 20:14:35 +0000

    openPDC.UI.WPF: Made changes to Output Stream Device Analog for binding data.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C70292

--- Source/Libraries/openPDC.UI.WPF/UserControls/OutputStreamDeviceAnalogUserControl.xaml.cs
+++ Source/Libraries/openPDC.UI.WPF/UserControls/OutputStreamDeviceAnalogUserControl.xaml.cs
@@ -1,16 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
+﻿//******************************************************************************************************
+//  OutputStreamDeviceAnalog.cs - Gbtc
+//
+//  Copyright © 2010, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the Eclipse Public License -v 1.0 (the "License"); you may
+//  not use this file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://www.opensource.org/licenses/eclipse-1.0.php
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  09/09/2011 - Aniket Salver
+//       Generated original version of source code.
+//
+//******************************************************************************************************
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using openPDC.UI.ViewModels;
+using openPDC.UI.DataModels;
 
 namespace openPDC.UI.UserControls
 {
@@ -19,9 +32,13 @@ namespace openPDC.UI.UserControls
     /// </summary>
     public partial class OutputStreamDeviceAnalogUserControl : UserControl
     {
+        /// <summary>
+        /// Creates an instance of <see cref=" OutputStreamDeviceAnalogUserControl"/> class.
+        /// </summary>
         public OutputStreamDeviceAnalogUserControl()
         {
             InitializeComponent();
+            this.DataContext = new OutputStreamDeviceAnalogs(1, true);
         }
 
         private void DataGrid_PreviewKeyDown(object sender, KeyEventArgs e)
