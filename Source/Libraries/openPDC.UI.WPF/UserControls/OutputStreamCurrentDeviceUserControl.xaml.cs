8c8f357952eb3fc6477d2074959ac6f353f545b2
salveraniket <salver.aniket@gmail.com>
2011-09-20 15:09:53 +0000

    openPDC.UI.WPF: Added OutputStreamCurrentDeviceUserControl class to User Controls
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C70513

---
+++ Source/Libraries/openPDC.UI.WPF/UserControls/OutputStreamCurrentDeviceUserControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace openPDC.UI.UserControls
+{
+    /// <summary>
+    /// Interaction logic for OutputStreamCurrentDeviceUserControl.xaml
+    /// </summary>
+    public partial class OutputStreamCurrentDeviceUserControl : UserControl
+    {
+        public OutputStreamCurrentDeviceUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
