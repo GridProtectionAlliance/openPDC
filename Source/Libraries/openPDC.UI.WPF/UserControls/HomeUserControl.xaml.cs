11ecf7a0637476d8e7e14c2f95dbf9a31dbd8e32
mthakkar <hello_mehul@yahoo.com>
2011-10-13 15:38:09 +0000

    openPDC.UI.WPF: Added a home user control with quick links. Modified menu.xml.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C71386

---
+++ Source/Libraries/openPDC.UI.WPF/UserControls/HomeUserControl.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Xml;
+using System.Xml.Serialization;
+using TimeSeriesFramework.UI;
+using TVA.IO;
+
+namespace openPDC.UI.UserControls
+{
+    /// <summary>
+    /// Interaction logic for HomeUserControl.xaml
+    /// </summary>
+    public partial class HomeUserControl : UserControl
+    {
+        #region [ Members ]
+
+        // Fields
+        private ObservableCollection<MenuDataItem> m_menuDataItems;
+        private WindowsServiceClient m_windowsServiceClient;
+
+        #endregion
+
+        #region [ Constructor ]
+
+        /// <summary>
+        /// Creates an instance of <see cref="HomeUserControl"/>.
+        /// </summary>
+        public HomeUserControl()
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(HomeUserControl_Loaded);
+            // Load Menu
+            XmlRootAttribute xmlRootAttribute = new XmlRootAttribute("MenuDataItems");
+            XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<MenuDataItem>), xmlRootAttribute);
+
+            using (XmlReader reader = XmlReader.Create(FilePath.GetAbsolutePath("Menu.xml")))
+            {
+                m_menuDataItems = (ObservableCollection<MenuDataItem>)serializer.Deserialize(reader);
+            }
+        }
+
+        #endregion
+
+        #region [ Methods ]
+
+        private void HomeUserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            m_windowsServiceClient = CommonFunctions.GetWindowsServiceClient();
+
+            if (m_windowsServiceClient == null || m_windowsServiceClient.Helper.RemotingClient.CurrentState != TVA.Communication.ClientState.Connected || !Thread.CurrentPrincipal.IsInRole("Administrator"))
+                ButtonRestart.IsEnabled = false;
+        }
+
+        private void ButtonQuickLink_Click(object sender, RoutedEventArgs e)
+        {
+            MenuDataItem item = new MenuDataItem();
+            string stringToMatch = ((Button)sender).Tag.ToString();
+
+            if (!string.IsNullOrEmpty(stringToMatch))
+            {
+                if (stringToMatch == "Restart")
+                {
+                    RestartService();
+                }
+                else
+                {
+                    GetMenuDataItem(m_menuDataItems, stringToMatch, ref item);
+
+                    if (item.MenuText != null)
+                    {
+                        item.Command.Execute(null);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Recursively finds menu item to navigate to when a button is clicked on the UI.
+        /// </summary>
+        /// <param name="items">Collection of menu items.</param>
+        /// <param name="stringToMatch">Item to search for in menu items collection.</param>
+        /// <param name="item">Returns a menu item.</param>
+        private void GetMenuDataItem(ObservableCollection<MenuDataItem> items, string stringToMatch, ref MenuDataItem item)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (items[i].MenuText.Contains(stringToMatch))
+                {
+                    item = items[i];
+                    break;
+                }
+                else
+                {
+                    if (items[i].SubMenuItems.Count > 0)
+                    {
+                        GetMenuDataItem(items[i].SubMenuItems, stringToMatch, ref item);
+                    }
+                }
+            }
+        }
+
+        private void RestartService()
+        {
+            if (MessageBox.Show("Do you want to restart openPDC service?", "Restart openPDC Service", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                if (m_windowsServiceClient != null && m_windowsServiceClient.Helper.RemotingClient.CurrentState == TVA.Communication.ClientState.Connected)
+                {
+                    CommonFunctions.SendCommandToService("Restart");
+                    MessageBox.Show("Successfully sent RESTART command to openPDC service.", "Restart openPDC Service", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Failed sent RESTART command to openPDC service." + Environment.NewLine + "openPDC service is either offline or disconnected.", "Restart openPDC Service", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
