04f1f3d3467c40068e5a38e0c82f0536b85e0566
mthakkar <hello_mehul@yahoo.com>
2011-11-10 19:17:42 +0000

    SQLite Data Script: Fixed StatisticMeasurement view.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C72160

--- Source/Data/SQLite/openPDC.sql
+++ Source/Data/SQLite/openPDC.sql
@@ -1079,9 +1079,7 @@ FROM Phasor P LEFT OUTER JOIN Phasor DP ON P.DestinationPhasorID = DP.ID
       LEFT OUTER JOIN Device D ON P.DeviceID = D.ID;
 
 CREATE VIEW StatisticMeasurement AS
-SELECT     MeasurementDetail.*, CASE WHEN CHARINDEX('!IS', SignalReference, 0) > 0 THEN 'InputStream' WHEN CHARINDEX('!OS', SignalReference, 0) > 0 THEN 'OutputStream' ELSE 'Device' END AS MeasurementSource
-FROM MeasurementDetail 
-WHERE MeasurementDetail.SignalAcronym = 'STAT';
+SELECT     MeasurementDetail.*, 'InputStream' AS MeasurementSource FROM MeasurementDetail WHERE MeasurementDetail.SignalAcronym = 'STAT' AND SignalReference LIKE '%IS%' UNION SELECT     MeasurementDetail.*, 'OutputStream' AS MeasurementSource FROM MeasurementDetail WHERE MeasurementDetail.SignalAcronym = 'STAT' AND SignalReference LIKE '%!OS%' UNION SELECT     MeasurementDetail.*, 'Device' AS MeasurementSource FROM MeasurementDetail WHERE MeasurementDetail.SignalAcronym = 'STAT' AND SignalReference NOT LIKE '%!IS%' AND SignalReference NOT LIKE '%!OS%';
 
 CREATE VIEW AppRoleSecurityGroupDetail AS 
 SELECT ApplicationRoleSecurityGroup.ApplicationRoleID AS ApplicationRoleID,ApplicationRoleSecurityGroup.SecurityGroupID AS SecurityGroupID,ApplicationRole.Name AS ApplicationRoleName,ApplicationRole.Description AS ApplicationRoleDescription,SecurityGroup.Name AS SecurityGroupName,SecurityGroup.Description AS SecurityGroupDescription 
