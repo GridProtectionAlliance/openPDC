90a2c5cc8b81aec5a2c282d738504628433228c9
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2011-12-20 05:04:06 +0000

    Synchrophasor: Version change for build Synchrophasor_v1.4.203.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C73540

--- Source/Libraries/Adapters/NASPInet/Properties/AssemblyInfo.cs
+++ Source/Libraries/Adapters/NASPInet/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using System.Security.Permissions;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.4.202.0")]
+[assembly: AssemblyVersion("1.4.203.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
