a65ca013fcc89b2037af8d10578e9f820686cc85
pinalcpatel <pcpatel@tva.gov>
2012-05-25 19:15:27 +0000

    Synchrophasor: Version change for build Synchrophasor_v1.5.88.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C78353

--- Source/Libraries/Adapters/NASPInet/Properties/AssemblyInfo.cs
+++ Source/Libraries/Adapters/NASPInet/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using System.Security.Permissions;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.5.87.0")]
+[assembly: AssemblyVersion("1.5.88.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
