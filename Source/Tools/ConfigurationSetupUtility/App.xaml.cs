aee269164e3631af29541c4451ab5cd7895fcb27
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2012-09-17 16:06:08 +0000

    Synchrophasor: Added code to to create an event log source in the Configuration Setup Utility at startup for the openPDC Manager for authentication logging. This needs to be done in the CSU since it runs with administrative privileges and the openPDC Manager normally does not; also there is a short system delay that exists before you can write to a new event log source after it is first created.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version;C79897

--- Source/Tools/ConfigurationSetupUtility/App.xaml.cs
+++ Source/Tools/ConfigurationSetupUtility/App.xaml.cs
@@ -24,9 +24,11 @@
 //******************************************************************************************************
 
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Security.Principal;
 using System.Windows;
+using System.Xml;
 using TVA.ErrorManagement;
 using TVA.IO;
 using TVA.Security.Cryptography;
@@ -69,6 +71,51 @@ namespace ConfigurationSetupUtility
 
             // When run from the installer the current directory may not be the directory where this application is running
             Directory.SetCurrentDirectory(FilePath.GetAbsolutePath(""));
+
+            // Attempt to create an event log source for the openPDC Manager for authentication logging. This needs to be done
+            // here since the CSU runs with administrative privileges and the openPDC Manager normally does not; also there is
+            // a short system delay that exists before you can write to a new event log source after it is first created.
+            try
+            {
+                // Attempt to load the application defined for the openPDC Manager in its configuration file
+                string configFileName = FilePath.GetAbsolutePath("openPDCManager.exe.config");
+
+                if (File.Exists(configFileName))
+                {
+                    XmlDocument configFile = new XmlDocument();
+                    configFile.Load(configFileName);
+
+                    XmlNode securitySettings = configFile.SelectSingleNode("configuration/categorizedSettings/securityProvider");
+
+                    if ((object)securitySettings != null)
+                    {
+                        string applicationName = null;
+
+                        foreach (XmlNode child in securitySettings.ChildNodes)
+                        {
+                            if ((object)child.Attributes != null && (object)child.Attributes["name"] != null)
+                            {
+                                if (child.Attributes["name"].Value == "ApplicationName")
+                                {
+                                    applicationName = child.Attributes["value"].Value;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (string.IsNullOrWhiteSpace(applicationName))
+                            applicationName = "openPDC Manager";
+
+                        // Create the event log source based on defined application name for openPDC Manager if it does not already exist
+                        if (!EventLog.SourceExists(applicationName))
+                            EventLog.CreateEventSource(applicationName, "Application");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                m_errorLogger.Log(new InvalidOperationException(string.Format("Warning: failed to create or validate the event log source for the openPDC Manager: {0}", ex.Message), ex), false);
+            }
         }
 
         #endregion
