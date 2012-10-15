8d0cddbdf32b3418f18d6a13cfaa9c76f0766f0d
StephenCWills <swills@gridprotectionalliance.org>
2012-10-15 19:22:04 +0000

    VS2012: Modified update dependencies script to receive updates from the Grid Solutions Framework build.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version-VS2012;C80436

--- Build/Scripts/UpdateDependencies.bat
+++ Build/Scripts/UpdateDependencies.bat
@@ -17,22 +17,16 @@
 
 SET vs="%VS100COMNTOOLS%\..\IDE\devenv.com"
 SET tfs="%VS100COMNTOOLS%\..\IDE\tf.exe"
-SET source1="\\GPAWEB\NightlyBuilds\TVACodeLibrary\Beta\Libraries\*.*"
-SET target1="..\..\Source\Dependencies\TVA"
-SET source2="\\GPAWEB\NightlyBuilds\TimeSeriesFramework\Beta\Libraries\*.*"
-SET target2="..\..\Source\Dependencies\TimeSeriesFramework"
-SET source3="\\GPAWEB\NightlyBuilds\openHistorian\Beta\Libraries\*.*"
-SET target3="..\..\Source\Dependencies\TVA"
+SET source1="\\GPAWEB\NightlyBuilds\GridSolutionsFramework\Beta\Libraries\*.*"
+SET target1="..\..\Source\Dependencies\GSF"
 SET solution="..\..\Source\Synchrophasor.sln"
 SET sourcetools=..\..\Source\Applications\openPDC\openPDCSetup\
-SET frameworktools=\\GPAWEB\NightlyBuilds\TVACodeLibrary\Beta\Tools\
-SET historiantools=\\GPAWEB\NightlyBuilds\openHistorian\Beta\Tools\
+SET frameworktools=\\GPAWEB\NightlyBuilds\GridSolutionsFramework\Beta\Tools\
 SET /p checkin=Check-in updates (Y or N)? 
 
 ECHO.
 ECHO Getting latest version...
 %tfs% get %target1% /version:T /force /recursive /noprompt
-%tfs% get %target2% /version:T /force /recursive /noprompt
 %tfs% get "%sourcetools%ConfigCrypter.exe" /force /recursive /noprompt
 %tfs% get "%sourcetools%ConfigurationEditor.exe" /force /recursive /noprompt
 %tfs% get "%sourcetools%DataMigrationUtility.exe" /force /recursive /noprompt
@@ -42,7 +36,6 @@ ECHO Getting latest version...
 ECHO.
 ECHO Checking out dependencies...
 %tfs% checkout %target1% /recursive /noprompt
-%tfs% checkout %target2% /recursive /noprompt
 %tfs% checkout "%sourcetools%ConfigCrypter.exe" /noprompt
 %tfs% checkout "%sourcetools%ConfigurationEditor.exe" /noprompt
 %tfs% checkout "%sourcetools%DataMigrationUtility.exe" /noprompt
@@ -51,14 +44,12 @@ ECHO Checking out dependencies...
 
 ECHO.
 ECHO Updating dependencies...
-XCOPY %source3% %target3% /Y /U
-XCOPY %source2% %target2% /Y /U
 XCOPY %source1% %target1% /Y /U
 XCOPY "%frameworktools%ConfigCrypter\ConfigCrypter.exe" "%sourcetools%ConfigCrypter.exe" /Y
 XCOPY "%frameworktools%ConfigEditor\ConfigEditor.exe" "%sourcetools%ConfigurationEditor.exe" /Y
 XCOPY "%frameworktools%DataMigrationUtility\DataMigrationUtility.exe" "%sourcetools%DataMigrationUtility.exe" /Y
-XCOPY "%historiantools%HistorianPlaybackUtility\HistorianPlaybackUtility.exe" "%sourcetools%HistorianPlaybackUtility.exe" /Y
-XCOPY "%historiantools%HistorianView\HistorianView.exe" "%sourcetools%HistorianView.exe" /Y
+XCOPY "%frameworktools%HistorianPlaybackUtility\HistorianPlaybackUtility.exe" "%sourcetools%HistorianPlaybackUtility.exe" /Y
+XCOPY "%frameworktools%HistorianView\HistorianView.exe" "%sourcetools%HistorianView.exe" /Y
 
 :: ECHO.
 :: ECHO Building solution...
@@ -71,7 +62,6 @@ GOTO Finalize
 ECHO.
 ECHO Checking in dependencies...
 %tfs% checkin %target1% /noprompt /recursive /comment:"Synchrophasor: Updated code library dependencies."
-%tfs% checkin %target2% /noprompt /recursive /comment:"Synchrophasor: Updated time-series framework dependencies."
 %tfs% checkin "%sourcetools%ConfigCrypter.exe" /noprompt /comment:"Synchrophasor: Updated code library tool: ConfigCrypter."
 %tfs% checkin "%sourcetools%ConfigurationEditor.exe" /noprompt /comment:"Synchrophasor: Updated code library tools: ConfigurationEditor."
 %tfs% checkin "%sourcetools%DataMigrationUtility.exe" /noprompt /comment:"Synchrophasor: Updated code library tools: DataMigrationUtility."
