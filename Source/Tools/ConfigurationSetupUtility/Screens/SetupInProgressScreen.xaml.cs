8ef7eede51b199df2455ec3318f235d894c4186a
StephenCWills <swills@gridprotectionalliance.org>
2013-07-11 18:49:04 +0000

    Synchrophasor-VS2012: In Configuration Setup Utility, added an exception for "Local System" account when setting up SQL Server security.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version-VS2012;C85979

--- Source/Tools/ConfigurationSetupUtility/Screens/SetupInProgressScreen.xaml.cs
+++ Source/Tools/ConfigurationSetupUtility/Screens/SetupInProgressScreen.xaml.cs
@@ -524,7 +524,7 @@ namespace ConfigurationSetupUtility.Screens
                             useGroupLogin = UserInfo.LocalGroupExists(groupName) && (host == "." || Transport.IsLocalAddress(host));
                             loginName = useGroupLogin ? string.Format(@"{0}\{1}", Environment.MachineName, groupName) : GetServiceAccountName();
 
-                            if ((object)loginName != null)
+                            if ((object)loginName != null && !loginName.Equals("Local System", StringComparison.InvariantCultureIgnoreCase))
                             {
                                 AppendStatusMessage(string.Format("Attempting to add Windows authenticated database login for {0}...", loginName));
 
