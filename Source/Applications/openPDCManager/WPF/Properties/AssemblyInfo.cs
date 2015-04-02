67c3968d73b5254423835c7cb01f4b4299024787
gsfbuildbot <support@gridprotectionalliance.org>
2015-04-02 04:14:00 +0000

    Synchrophasor: Version change for build Synchrophasor_v2.1.78.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openpdc/Synchrophasor/Current Version-VS2012;C95034

--- Source/Applications/openPDCManager/WPF/Properties/AssemblyInfo.cs
+++ Source/Applications/openPDCManager/WPF/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 using System.Windows;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.1.77.0")]
+[assembly: AssemblyVersion("2.1.78.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
