91d588cf938b909ac44b3b46c53ec9edcc579a6c
StephenCWills <swills@gridprotectionalliance.org>
2016-05-13 17:04:02 -0400

    Modified the ConfigurationSetupUtility to force LogToDatabase to false when using SQLite.

--- Source/Tools/ConfigurationSetupUtility/Screens/SetupInProgressScreen.xaml.cs
+++ Source/Tools/ConfigurationSetupUtility/Screens/SetupInProgressScreen.xaml.cs
@@ -1859,7 +1859,7 @@ namespace ConfigurationSetupUtility.Screens
                     addElement.Attributes.Append(attribute);
 
                     attribute = configFile.CreateAttribute("value");
-                    attribute.Value = "True";
+                    attribute.Value = (m_state["newDatabaseType"].ToString() != "SQLite").ToString();
                     addElement.Attributes.Append(attribute);
 
                     attribute = configFile.CreateAttribute("description");
@@ -1871,6 +1871,10 @@ namespace ConfigurationSetupUtility.Screens
                     addElement.Attributes.Append(attribute);
 
                     errorLoggerNode.AppendChild(addElement);
+                }
+                else if (m_state["newDatabaseType"].ToString() == "SQLite")
+                {
+                    logToDatabaseNode.Attributes["value"].Value = "False";
                 }
             }
 
