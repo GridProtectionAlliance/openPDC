5b3b30b027b9fbbe75365f8a6640fdc9652d37f7
StephenCWills <swills@gridprotectionalliance.org>
2016-05-13 10:05:54 -0400

    Updated the DebugHost icon.

--- Source/Applications/openPDC/openPDC/DebugHost.cs
+++ Source/Applications/openPDC/openPDC/DebugHost.cs
@@ -39,5 +39,20 @@ namespace openPDC
                 return "openPDCConsole.exe";
             }
         }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DebugHost));
+            this.SuspendLayout();
+            // 
+            // DebugHost
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(344, 73);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "DebugHost";
+            this.ResumeLayout(false);
+
+        }
     }
 }
