9eca15283d370b5a006d7979b7e5312cba2dcccb
Billy Ernest <bernest@gridprotectionalliance.org>
2018-01-12 10:34:01 -0500

    Added sql openPDC specific sql scripts.

---
+++ Source/Data/PostgreSQL/_openPDC.sql
@@ -0,0 +1,37 @@
+-- *******************************************************************************************
+-- IMPORTANT NOTE: When making updates to this schema, please increment the version number!
+-- *******************************************************************************************
+CREATE VIEW LocalSchemaVersion AS
+SELECT 1 AS VersionNumber;
+
+
+CREATE TABLE DataAvailability(
+	ID SERIAL NOT NULL PRIMARY KEY,
+	GoodAvailableData DOUBLE PRECISION NOT NULL,
+	BadAvailableData DOUBLE PRECISION NOT NULL,
+	TotalAvailableData DOUBLE PRECISION NOT NULL,
+);
+
+CREATE TABLE AlarmDevice(
+	ID SERIAL NOT NULL PRIMARY KEY,
+	DeviceID INTEGER NULL FOREIGN KEY REFERENCES Device(ID),
+	StateID INTEGER NULL FOREIGN KEY REFERENCES AlarmState(ID),
+	TimeStamp TIMESTAMP NULL,
+	DisplayData varchar(10) NULL,
+	CONSTRAINT FK_AlarmDevice_Device FOREIGN KEY(DeviceID) REFERENCES Device (ID) ON DELETE CASCADE,
+    CONSTRAINT FK_AlarmDevice_AlarmState FOREIGN KEY(StateID) REFERENCES AlarmState (ID) ON DELETE CASCADE
+
+);
+
+CREATE TABLE AlarmState(
+	ID SERIAL NOT NULL PRIMARY KEY,
+	State varchar(50) NULL,
+	Color varchar(50) NULL,
+);
+
+INSERT INTO AlarmState (State, Color) VALUES ('Good', 'green');
+INSERT INTO AlarmState (State, Color) VALUES ('Alarm', 'red');
+INSERT INTO AlarmState (State, Color) VALUES ('Not Available', 'orange')
+INSERT INTO AlarmState (State, Color) VALUES ('Bad Data', 'blue');
+INSERT INTO AlarmState (State, Color) VALUES ('Bad Time', 'purple');
+INSERT INTO AlarmState (State, Color) VALUES ('Out of Service', 'grey');
