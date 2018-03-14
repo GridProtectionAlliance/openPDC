5e18f034e6ccbab664e41210c00e74dccdbff24f
gsfbuildbot <support@gridprotectionalliance.org>
2018-03-14 00:02:28 -0400

    Updated GSF dependencies.

--- Source/Data/SQL Server/openPDC.sql
+++ Source/Data/SQL Server/openPDC.sql
@@ -3135,3 +3135,49 @@ CREATE TABLE AlarmDevice(
 	DisplayData varchar(10) NULL
 )
 GO
+ 
+-- *******************************************************************************************
+-- IMPORTANT NOTE: When making updates to this schema, please increment the version number!
+-- *******************************************************************************************
+CREATE VIEW [dbo].[LocalSchemaVersion] AS
+SELECT 1 AS VersionNumber
+GO
+
+CREATE TABLE DataAvailability(
+	ID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	GoodAvailableData float NOT NULL,
+	BadAvailableData float NOT NULL,
+	TotalAvailableData float NOT NULL,
+)
+
+GO
+
+CREATE TABLE AlarmState(
+	ID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	State varchar(50) NULL,
+	Color varchar(50) NULL,
+)
+
+GO
+
+INSERT AlarmState (State, Color) VALUES ('Good', 'green')
+GO
+INSERT AlarmState (State, Color) VALUES ('Alarm', 'red')
+GO
+INSERT AlarmState (State, Color) VALUES ('Not Available', 'orange')
+GO
+INSERT AlarmState (State, Color) VALUES ('Bad Data', 'blue')
+GO
+INSERT AlarmState (State, Color) VALUES ('Bad Time', 'purple')
+GO
+INSERT AlarmState (State, Color) VALUES ('Out of Service', 'grey')
+GO
+
+CREATE TABLE AlarmDevice(
+	ID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	DeviceID int NULL FOREIGN KEY REFERENCES Device(ID),
+	StateID int NULL FOREIGN KEY REFERENCES AlarmState(ID),
+	TimeStamp datetime NULL,
+	DisplayData varchar(10) NULL
+)
+GO
--- Source/Data/SQLite/openPDC.sql
+++ Source/Data/SQLite/openPDC.sql
@@ -1749,3 +1749,40 @@ CREATE TABLE AlarmDevice(
 	CONSTRAINT FK_AlarmDevice_Device FOREIGN KEY(DeviceID) REFERENCES Device (ID) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT FK_AlarmDevice_AlarmState FOREIGN KEY(StateID) REFERENCES AlarmState (ID) ON DELETE CASCADE ON UPDATE CASCADE
 );
+ 
+-- *******************************************************************************************
+-- IMPORTANT NOTE: When making updates to this schema, please increment the version number!
+-- *******************************************************************************************
+CREATE VIEW LocalSchemaVersion AS
+SELECT 1 AS VersionNumber;
+
+CREATE TABLE DataAvailability(
+	ID int PRIMARY KEY AUTOINCREMENT NOT NULL,
+	GoodAvailableData DOUBLE NOT NULL,
+	BadAvailableData DOUBLE NOT NULL,
+	TotalAvailableData DOUBLE NOT NULL
+);
+
+CREATE TABLE AlarmState(
+	ID int PRIMARY KEY AUTOINCREMENT NOT NULL,
+	State varchar(50) NULL,
+	Color varchar(50) NULL,
+);
+
+INSERT INTO AlarmState (State, Color) VALUES ('Good', 'green');
+INSERT INTO AlarmState (State, Color) VALUES ('Alarm', 'red');
+INSERT INTO AlarmState (State, Color) VALUES ('Not Available', 'orange')
+INSERT INTO AlarmState (State, Color) VALUES ('Bad Data', 'blue');
+INSERT INTO AlarmState (State, Color) VALUES ('Bad Time', 'purple');
+INSERT INTO AlarmState (State, Color) VALUES ('Out of Service', 'grey');
+
+
+CREATE TABLE AlarmDevice(
+	ID int PRIMARY KEY AUTOINCREMENT NOT NULL,
+	DeviceID int NULL,
+	StateID int NULL,
+	TimeStamp datetime NULL,
+	DisplayData varchar(10) NULL,
+	CONSTRAINT FK_AlarmDevice_Device FOREIGN KEY(DeviceID) REFERENCES Device (ID) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT FK_AlarmDevice_AlarmState FOREIGN KEY(StateID) REFERENCES AlarmState (ID) ON DELETE CASCADE ON UPDATE CASCADE
+);
