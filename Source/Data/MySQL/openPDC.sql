cc820d0e5d1e83791d7f5c2fd8d82b98fcd53a03
gsfbuildbot <support@gridprotectionalliance.org>
2018-05-09 15:26:58 -0400

    Updated GSF dependencies.

--- Source/Data/MySQL/openPDC.sql
+++ Source/Data/MySQL/openPDC.sql
@@ -1831,13 +1831,6 @@ CREATE TABLE AlarmState(
 	PRIMARY KEY(ID)
 );
 
-INSERT INTO AlarmState (State, Color) VALUES ('Good', 'green');
-INSERT INTO AlarmState (State, Color) VALUES ('Alarm', 'red');
-INSERT INTO AlarmState (State, Color) VALUES ('Not Available', 'orange')
-INSERT INTO AlarmState (State, Color) VALUES ('Bad Data', 'blue');
-INSERT INTO AlarmState (State, Color) VALUES ('Bad Time', 'purple');
-INSERT INTO AlarmState (State, Color) VALUES ('Out of Service', 'grey');
-
 CREATE TABLE AlarmDevice(
 	ID int AUTO_INCREMENT NOT NULL,
 	DeviceID int NULL,
--- Source/Data/SQL Server/InitialDataSet.sql
+++ Source/Data/SQL Server/InitialDataSet.sql
@@ -717,3 +717,23 @@ GO
 
 INSERT INTO VendorDevice(VendorID, Name, Description, URL) VALUES(15, 'ePDC', 'ePDC & eSPDC', 'http://www.electricpowergroup.com/solutions/epdc/index.html')
 GO
+USE openPDC
+GO
+
+INSERT AlarmState (State, Color) VALUES ('Good', 'green')
+GO
+
+INSERT AlarmState (State, Color) VALUES ('Alarm', 'red')
+GO
+
+INSERT AlarmState (State, Color) VALUES ('Not Available', 'orange')
+GO
+
+INSERT AlarmState (State, Color) VALUES ('Bad Data', 'blue')
+GO
+
+INSERT AlarmState (State, Color) VALUES ('Bad Time', 'purple')
+GO
+
+INSERT AlarmState (State, Color) VALUES ('Out of Service', 'grey')
+GO
