33085675765a357c5c81e57d2d07485c9845278d
StephenCWills <swills@gridprotectionalliance.org>
2018-05-16 11:19:33 -0400

    Added missing semicolon in SQLite _InitialDataSet.sql script.

--- Source/Data/SQLite/_InitialDataSet.sql
+++ Source/Data/SQLite/_InitialDataSet.sql
@@ -2,7 +2,7 @@ PRAGMA foreign_keys = ON;
 
 INSERT INTO AlarmState (State, Color) VALUES ('Good', 'green');
 INSERT INTO AlarmState (State, Color) VALUES ('Alarm', 'red');
-INSERT INTO AlarmState (State, Color) VALUES ('Not Available', 'orange')
+INSERT INTO AlarmState (State, Color) VALUES ('Not Available', 'orange');
 INSERT INTO AlarmState (State, Color) VALUES ('Bad Data', 'blue');
 INSERT INTO AlarmState (State, Color) VALUES ('Bad Time', 'purple');
 INSERT INTO AlarmState (State, Color) VALUES ('Out of Service', 'grey');
