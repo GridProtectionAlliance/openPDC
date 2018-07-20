e0de1fdaecbacf7d540da53d1904b0584eee87aa
StephenCWills <swills@gridprotectionalliance.org>
2018-07-20 14:58:38 -0400

    Removed INSERT statements from _openPDC.sql which had already been moved to _InitialDataSet.sql.

--- Source/Data/SQL Server/_openPDC.sql
+++ Source/Data/SQL Server/_openPDC.sql
@@ -20,24 +20,6 @@ CREATE TABLE AlarmState(
 )
 GO
 
-INSERT INTO AlarmState(State, Color) VALUES('Good', 'green')
-GO
-
-INSERT INTO AlarmState(State, Color) VALUES('Alarm', 'red')
-GO
-
-INSERT INTO AlarmState(State, Color) VALUES('Not Available', 'orange')
-GO
-
-INSERT INTO AlarmState(State, Color) VALUES('Bad Data', 'blue')
-GO
-
-INSERT INTO AlarmState(State, Color) VALUES('Bad Time', 'purple')
-GO
-
-INSERT INTO AlarmState(State, Color) VALUES('Out of Service', 'grey')
-GO
-
 CREATE TABLE AlarmDevice(
 	ID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	DeviceID int NULL FOREIGN KEY REFERENCES Device(ID),
