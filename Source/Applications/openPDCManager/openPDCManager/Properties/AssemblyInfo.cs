20128161d0bc0661148518278d53c3496230cfe2
gsfbuildbot <support@gridprotectionalliance.org>
2019-04-08 00:19:52 -0400

    Synchrophasor: Version change for build v2.6.111.0-master.

--- Source/Applications/openPDCManager/openPDCManager/Properties/AssemblyInfo.cs
+++ Source/Applications/openPDCManager/openPDCManager/Properties/AssemblyInfo.cs
@@ -53,6 +53,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.6.110.0")]
-[assembly: AssemblyVersion("2.6.110.0")]
-[assembly: AssemblyFileVersion("2.6.110.0")]
+// [assembly: AssemblyVersion("2.6.111.0")]
+[assembly: AssemblyVersion("2.6.111.0")]
+[assembly: AssemblyFileVersion("2.6.111.0")]
