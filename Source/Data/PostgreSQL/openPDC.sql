08275924a0e6adb14b32664d69a28c132a60461a
gsfbuildbot <support@gridprotectionalliance.org>
2019-04-16 10:52:27 -0400

    Updated GSF dependencies.

--- Source/Data/PostgreSQL/openPDC.sql
+++ Source/Data/PostgreSQL/openPDC.sql
@@ -1934,4 +1934,38 @@ CREATE VIEW AlarmDeviceStateView AS
 SELECT AlarmDevice.ID, Device.Name, AlarmState.State, AlarmState.Color, AlarmDevice.DisplayData
 FROM AlarmDevice
     INNER JOIN AlarmState ON AlarmDevice.StateID = AlarmState.ID
+    INNER JOIN Device ON AlarmDevice.DeviceID = Device.ID; 
+-- *******************************************************************************************
+-- IMPORTANT NOTE: When making updates to this schema, please increment the version number!
+-- *******************************************************************************************
+CREATE VIEW LocalSchemaVersion AS
+SELECT 2 AS VersionNumber;
+
+CREATE TABLE DataAvailability(
+	ID SERIAL NOT NULL PRIMARY KEY,
+	GoodAvailableData DOUBLE PRECISION NOT NULL,
+	BadAvailableData DOUBLE PRECISION NOT NULL,
+	TotalAvailableData DOUBLE PRECISION NOT NULL
+);
+
+CREATE TABLE AlarmState(
+	ID SERIAL NOT NULL PRIMARY KEY,
+	State varchar(50) NULL,
+	Color varchar(50) NULL
+);
+
+CREATE TABLE AlarmDevice(
+	ID SERIAL NOT NULL PRIMARY KEY,
+	DeviceID INTEGER NULL,
+	StateID INTEGER NULL,
+	TimeStamp TIMESTAMP NULL,
+	DisplayData varchar(10) NULL,
+	CONSTRAINT FK_AlarmDevice_Device FOREIGN KEY(DeviceID) REFERENCES Device (ID) ON DELETE CASCADE,
+    CONSTRAINT FK_AlarmDevice_AlarmState FOREIGN KEY(StateID) REFERENCES AlarmState (ID) ON DELETE CASCADE
+);
+
+CREATE VIEW AlarmDeviceStateView AS
+SELECT AlarmDevice.ID, Device.Name, AlarmState.State, AlarmState.Color, AlarmDevice.DisplayData
+FROM AlarmDevice
+    INNER JOIN AlarmState ON AlarmDevice.StateID = AlarmState.ID
     INNER JOIN Device ON AlarmDevice.DeviceID = Device.ID;
