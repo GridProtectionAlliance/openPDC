69e93d1eca45e6e35eb762130d7b628844530d79
gsfbuildbot <support@gridprotectionalliance.org>
2019-12-10 19:31:37 -0500

    Synchrophasor: Version change for build v2.8.63.0-master.

--- Source/Applications/openPDCManager/Silverlight/Properties/AssemblyInfo.cs
+++ Source/Applications/openPDCManager/Silverlight/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.8.62.0")]
-[assembly: AssemblyFileVersion("2.8.62.0")]
+[assembly: AssemblyVersion("2.8.63.0")]
+[assembly: AssemblyFileVersion("2.8.63.0")]
