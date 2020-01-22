1bdc8371d9308b8d6aef8eacf936576b1ae45013
Christoph Lackner <clackner@gridprotectionalliance.org>
2020-01-22 14:11:01 -0500

    Moved default Alarmstates to GSF

--- Source/Data/SQL Server/_InitialDataSet.sql
+++ Source/Data/SQL Server/_InitialDataSet.sql
@@ -1,23 +1,2 @@
 USE openPDC
 GO
-
-INSERT INTO AlarmState (State, Color) VALUES ('Good', 'green')
-GO
-
-INSERT INTO AlarmState (State, Color) VALUES ('Alarm', 'red')
-GO
-
-INSERT INTO AlarmState (State, Color) VALUES ('Not Available', 'orange')
-GO
-
-INSERT INTO AlarmState (State, Color) VALUES ('Bad Data', 'blue')
-GO
-
-INSERT INTO AlarmState (State, Color) VALUES ('Bad Time', 'purple')
-GO
-
-INSERT INTO AlarmState (State, Color) VALUES ('Out of Service', 'grey')
-GO
-
-INSERT INTO AlarmState (State, Color) VALUES ('Acknowledged', 'rosybrown')
-GO
