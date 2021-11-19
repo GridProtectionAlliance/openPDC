fd7ccbb5494b9768b890f3b0ad62360931ca80d0
StephenCWills <swills@gridprotectionalliance.org>
2021-11-19 16:06:56 -0500

    Oracle database script cleanup

--- Source/Data/Oracle/_openPDC.sql
+++ Source/Data/Oracle/_openPDC.sql
@@ -5,13 +5,15 @@ CREATE VIEW LocalSchemaVersion AS
 SELECT 3 AS VersionNumber
 FROM dual;
 
-
 CREATE TABLE DataAvailability(
 	ID NUMBER NOT NULL,
 	GoodAvailableData NUMBER NOT NULL,
 	BadAvailableData NUMBER NOT NULL,
-	TotalAvailableData NUMBER NOT NULL,
+	TotalAvailableData NUMBER NOT NULL
 );
+
+CREATE UNIQUE INDEX IX_DataAvailability_ID ON DataAvailability (ID ASC) TABLESPACE openPDC_INDEX;
+
 ALTER TABLE DataAvailability ADD CONSTRAINT PK_DataAvailability PRIMARY KEY (ID);
 
 CREATE SEQUENCE SEQ_DataAvailability START WITH 1 INCREMENT BY 1;
@@ -19,3 +21,4 @@ CREATE SEQUENCE SEQ_DataAvailability START WITH 1 INCREMENT BY 1;
 CREATE TRIGGER AI_DataAvailability BEFORE INSERT ON DataAvailability
     FOR EACH ROW BEGIN SELECT SEQ_DataAvailability.nextval INTO :NEW.ID FROM dual;
 END;
+/
