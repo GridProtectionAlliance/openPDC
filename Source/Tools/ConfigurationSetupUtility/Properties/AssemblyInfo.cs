4613262ff62cd60f7d14e0b2e35b599de9862f9e
gsfbuildbot <support@gridprotectionalliance.org>
2022-11-15 00:22:19 -0500

    Synchrophasor: Version change for build v2.9.203.0-master.

--- Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
@@ -54,6 +54,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.9.202.0")]
-[assembly: AssemblyVersion("2.9.202.0")]
-[assembly: AssemblyFileVersion("2.9.202.0")]
+// [assembly: AssemblyVersion("2.9.203.0")]
+[assembly: AssemblyVersion("2.9.203.0")]
+[assembly: AssemblyFileVersion("2.9.203.0")]
