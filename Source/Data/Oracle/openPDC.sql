74b0e2f0bcb0ec50a676a18d1cf11b6bc4027d87
gsfbuildbot <support@gridprotectionalliance.org>
2023-10-17 00:04:06 -0400

    Updated GSF dependencies.

--- Source/Data/Oracle/openPDC.sql
+++ Source/Data/Oracle/openPDC.sql
@@ -2837,4 +2837,28 @@ CREATE SEQUENCE SEQ_DataAvailability START WITH 1 INCREMENT BY 1;
 CREATE TRIGGER AI_DataAvailability BEFORE INSERT ON DataAvailability
     FOR EACH ROW BEGIN SELECT SEQ_DataAvailability.nextval INTO :NEW.ID FROM dual;
 END;
+/ 
+-- *******************************************************************************************
+-- IMPORTANT NOTE: When making updates to this schema, please increment the version number!
+-- *******************************************************************************************
+CREATE VIEW LocalSchemaVersion AS
+SELECT 3 AS VersionNumber
+FROM dual;
+
+CREATE TABLE DataAvailability(
+	ID NUMBER NOT NULL,
+	GoodAvailableData NUMBER NOT NULL,
+	BadAvailableData NUMBER NOT NULL,
+	TotalAvailableData NUMBER NOT NULL
+);
+
+CREATE UNIQUE INDEX IX_DataAvailability_ID ON DataAvailability (ID ASC) TABLESPACE openPDC_INDEX;
+
+ALTER TABLE DataAvailability ADD CONSTRAINT PK_DataAvailability PRIMARY KEY (ID);
+
+CREATE SEQUENCE SEQ_DataAvailability START WITH 1 INCREMENT BY 1;
+
+CREATE TRIGGER AI_DataAvailability BEFORE INSERT ON DataAvailability
+    FOR EACH ROW BEGIN SELECT SEQ_DataAvailability.nextval INTO :NEW.ID FROM dual;
+END;
 /
