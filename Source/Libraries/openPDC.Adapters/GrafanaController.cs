fe7bfe4752c94c6dee47098b3f535f01c9f9e9f4
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2024-02-05 22:18:39 -0600

    Updated archive file name dictionary in GrafanaAdapters to use a concurrent dictionary

--- Source/Libraries/openPDC.Adapters/GrafanaController.cs
+++ Source/Libraries/openPDC.Adapters/GrafanaController.cs
@@ -41,6 +41,7 @@ using GSF.Web.Security;
 using HistorianAdapters;
 using openPDC.Model;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -97,27 +98,24 @@ namespace openPDC.Adapters
                 m_archiveReader?.Dispose();
             }
 
-            private static readonly Dictionary<string, string> s_archiveFileNames = new();
+            private static readonly ConcurrentDictionary<string, string> s_archiveFileNames = new();
 
             private static string GetArchiveFileName(string instanceName)
             {
                 instanceName = instanceName.ToLowerInvariant();
 
-                lock (s_archiveFileNames)
-                {
-                    if (s_archiveFileNames.TryGetValue(instanceName, out string archiveFileName))
-                        return archiveFileName;
-
-                    CategorizedSettingsElementCollection settings = ConfigurationFile.Current.Settings[$"{instanceName}ArchiveFile"];
-                    archiveFileName = settings?["FileName"]?.Value;
-
-                    if (string.IsNullOrWhiteSpace(archiveFileName))
-                        archiveFileName = instanceName.Equals("stat") ? @"Statistics\stat_archive.d" : $@"{instanceName}\{instanceName}_archive.d";
-
-                    s_archiveFileNames[instanceName] = archiveFileName;
-
+                if (s_archiveFileNames.TryGetValue(instanceName, out string archiveFileName))
                     return archiveFileName;
-                }
+
+                CategorizedSettingsElementCollection settings = ConfigurationFile.Current.Settings[$"{instanceName}ArchiveFile"];
+                archiveFileName = settings?["FileName"]?.Value;
+
+                if (string.IsNullOrWhiteSpace(archiveFileName))
+                    archiveFileName = instanceName.Equals("stat") ? @"Statistics\stat_archive.d" : $@"{instanceName}\{instanceName}_archive.d";
+
+                s_archiveFileNames[instanceName] = archiveFileName;
+
+                return archiveFileName;
             }
         }
 
