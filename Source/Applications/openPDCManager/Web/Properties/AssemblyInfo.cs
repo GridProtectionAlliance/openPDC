b0380382ebdd657503fb97727c8b163234d3a16c
gsfbuildbot <support@gridprotectionalliance.org>
2024-08-21 00:11:59 -0400

    Synchrophasor: Version change for build v2.9.333.0-master.

--- Source/Applications/openPDCManager/Web/Properties/AssemblyInfo.cs
+++ Source/Applications/openPDCManager/Web/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.9.332.0")]
-[assembly: AssemblyFileVersion("2.9.332.0")]
+[assembly: AssemblyVersion("2.9.333.0")]
+[assembly: AssemblyFileVersion("2.9.333.0")]
