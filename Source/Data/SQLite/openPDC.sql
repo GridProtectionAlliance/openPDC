e9f78af4f52ccabfa812fe968032f36b4a509698
gsfbuildbot <support@gridprotectionalliance.org>
2025-08-16 01:31:24 -0400

    Updated GSF dependencies.

--- Source/Data/SQLite/openPDC.sql
+++ Source/Data/SQLite/openPDC.sql
@@ -1771,17 +1771,3 @@ CREATE TABLE DataAvailability(
 	TotalAvailableData DOUBLE NOT NULL
 );
 
- 
--- *******************************************************************************************
--- IMPORTANT NOTE: When making updates to this schema, please increment the version number!
--- *******************************************************************************************
-CREATE VIEW LocalSchemaVersion AS
-SELECT 3 AS VersionNumber;
-
-CREATE TABLE DataAvailability(
-	ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	GoodAvailableData DOUBLE NOT NULL,
-	BadAvailableData DOUBLE NOT NULL,
-	TotalAvailableData DOUBLE NOT NULL
-);
-
