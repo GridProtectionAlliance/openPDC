63d0c0b906d0015e2a7690221274004be39a051d
gsfbuildbot <support@gridprotectionalliance.org>
2026-01-31 00:04:39 -0500

    Synchrophasor: Version change for build v2.9.443.0-master.

--- Source/Applications/openPDCManager/WPF/Properties/AssemblyInfo.cs
+++ Source/Applications/openPDCManager/WPF/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 using System.Windows;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("2.9.442.0")]
+[assembly: AssemblyVersion("2.9.443.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
