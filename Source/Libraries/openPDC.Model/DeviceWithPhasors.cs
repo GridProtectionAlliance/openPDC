fff5e7e75bfbed2318edbbef7b44e8df6da4ed94
Eduardo MO. de Córdova <eduardo.amcom@ons.org.br>
2026-05-21 23:51:28 -0300

    Adds the DeviceWithPhasors class to the model.
    Includes the DeviceWithPhasors class as a DTO to represent a Device (PMU) with its list of associated Phasors. Adds the new DeviceWithPhasors.cs file to the openPDC.Model.csproj project.

---
+++ Source/Libraries/openPDC.Model/DeviceWithPhasors.cs
@@ -0,0 +1,31 @@
+﻿// ReSharper disable CheckNamespace
+#pragma warning disable 1591
+
+using System.Collections.Generic;
+
+namespace openPDC.Model
+{
+    /// <summary>
+    /// DTO que representa um Device com seus Phasors associados.
+    /// </summary>
+    public class DeviceWithPhasors
+    {
+        /// <summary>
+        /// Construtor padrão.
+        /// </summary>
+        public DeviceWithPhasors()
+        {
+            Phasors = new List<PhasorDetail>();
+        }
+
+        /// <summary>
+        /// Informações do Device (PMU).
+        /// </summary>
+        public DeviceDetail Device { get; set; }
+
+        /// <summary>
+        /// Lista de Phasors associados ao Device.
+        /// </summary>
+        public List<PhasorDetail> Phasors { get; set; }
+    }
+}
